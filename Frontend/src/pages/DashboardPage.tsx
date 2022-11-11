ef4aaa90942edaa0bae71b46ced503409c7ccc6f
wish0ne <wish@khu.ac.kr>
2022-11-12 01:18:45 +0900

    feat: Naver Map API 연결

--- Frontend/src/pages/DashboardPage.tsx
+++ Frontend/src/pages/DashboardPage.tsx
@@ -2,11 +2,24 @@ import styled from 'styled-components';
 import palette from '../lib/palette';
 import Header from '../components/common/Header';
 import Landing from '../assets/images/landing.jpg';
+import { useEffect } from 'react';
+
+const { naver } = window;
+
+const center: naver.maps.LatLng = new naver.maps.LatLng(37.503545, 127.044878);
+const mapOptions = {
+  center: center,
+  zoom: 16,
+};
 
 const DashboardPage = () => {
+  useEffect(() => {
+    const map: naver.maps.Map = new window.naver.maps.Map('map', mapOptions);
+  }, []);
   return (
     <>
       <Header>도움말</Header>
+      <div id="map" style={{ width: '100%', height: '100%' }} />
     </>
   );
 };
