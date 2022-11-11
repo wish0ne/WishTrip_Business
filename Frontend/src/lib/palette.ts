4e0b2a1f81e36256e46a37c67131ad42e7576adc
wish0ne <wish@khu.ac.kr>
2022-11-11 19:53:41 +0900

    style: palette 추가

---
+++ Frontend/src/lib/palette.ts
@@ -0,0 +1,17 @@
+const palette = {
+  primary2: '#0eb6fe',
+  primary1: '#00d8ec',
+  primary3: '#0086e7',
+  inversed2: '#eeeeee',
+  inversed1: '#fafafa',
+  default2: '#4a4a4a',
+  inversed3: '#cdcdcd',
+  default1: '#707070',
+  secondary2: '#84f32d',
+  default3: '#1a1a1a',
+  secondary1: '#9df557',
+  red: '#f24125',
+  secondary3: '#6be30d',
+};
+
+export default palette;
