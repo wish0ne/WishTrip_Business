544f0395bbf9c41d5152d50645306138bfa71c91
wish0ne <wish@khu.ac.kr>
2022-11-22 23:23:34 +0900

    feat: get category/main 로직 수정, get category/middle 추가

--- Backend/src/api/store/index.js
+++ Backend/src/api/store/index.js
@@ -1,8 +1,10 @@
 import Router from "koa-router";
-import { readMainNames } from "./store.ctrl.js";
+import { readMainNames, readMiddleNames } from "./store.ctrl.js";
 
 const store = new Router();
 
 store.get("/category/main", readMainNames);
+
+store.get("/category/middle/:code", readMiddleNames); //params: main_code, return : main_code에 해당하는 {middle_code, middle_name}
 
 export default store;
--- Backend/src/api/store/store.ctrl.js
+++ Backend/src/api/store/store.ctrl.js
@@ -2,12 +2,52 @@ import Store from "../../models/store.js";
 
 export const readMainNames = async (ctx) => {
   try {
-    const stores = await Store.find().distinct("main_name").exec();
-    if (!stores) {
+    const codes = await Store.find().distinct("main_code").exec();
+    const result = await Promise.all(
+      codes.map(async (code) => {
+        const { main_name } = await Store.findOne(
+          { main_code: code },
+          { main_name: 1, _id: 0 }
+        ).exec();
+        return {
+          main_code: code,
+          main_name: main_name,
+        };
+      })
+    );
+    if (!codes) {
       ctx.status = 404;
       return;
     }
-    ctx.body = stores;
+    ctx.body = result;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const readMiddleNames = async (ctx) => {
+  const { code } = ctx.params;
+  try {
+    const middles_code = await Store.distinct("middle_code", {
+      middle_code: { $regex: code },
+    }).exec();
+    const result = await Promise.all(
+      middles_code.map(async (code) => {
+        const { middle_name } = await Store.findOne(
+          { middle_code: code },
+          { middle_name: 1, _id: 0 }
+        ).exec();
+        return {
+          middle_code: code,
+          middle_name: middle_name,
+        };
+      })
+    );
+    if (!middles_code) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = result;
   } catch (e) {
     ctx.throw(500, e);
   }
