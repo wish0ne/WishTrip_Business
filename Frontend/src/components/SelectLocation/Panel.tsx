cd2b587a2fcf8292356558548a2037c18a302b7b
wish0ne <wish@khu.ac.kr>
2022-11-22 21:44:46 +0900

    feat: 상권 카테고리 ui 추가

--- Frontend/src/components/SelectLocation/Panel.tsx
+++ Frontend/src/components/SelectLocation/Panel.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import palette from '../../lib/palette';
 import Title from '../common/Title';
 import Button from '../common/Button';
+import Selectbox from './Selectbox';
 
 const PanelBlock = styled.div`
   width: 50%;
@@ -14,15 +15,40 @@ const PanelBlock = styled.div`
     color: ${palette.default1};
     margin-bottom: 0.4rem;
   }
+  & > h3 {
+    font-family: 'Regular';
+    font-size: 1.4rem;
+    line-height: 2rem;
+    color: ${palette.primary3};
+  }
 `;
+
+const SelectContainer = styled.div`
+  display: flex;
+  gap: 0.8rem;
+`;
+
+const Line = styled.div`
+  width: 12rem;
+  height: 0.1rem;
+  background-color: ${palette.inversed2};
+  margin: 2.4rem 0;
+`;
+
+const bigOptions = [''];
 
 const Panel = () => {
   return (
     <PanelBlock>
       <Title>광고 지역을 선택하세요.</Title>
       <h2>분류 선택</h2>
-      <h2>광고 지역</h2>
-      <h2>광고 반경 선택</h2>
+      <SelectContainer>
+        <Selectbox options={bigOptions}>대분류</Selectbox>
+        <Selectbox>중분류</Selectbox>
+        <Selectbox>소분류</Selectbox>
+      </SelectContainer>
+      <Line />
+      <h3>지도에서 원하는 광고 지역을 선택해주세요.</h3>
       <Button active={false}>다음</Button>
     </PanelBlock>
   );
---
+++ Frontend/src/components/SelectLocation/Selectbox.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+import palette from '../../lib/palette';
+import { ReactComponent as Down } from '../../assets/images/menu-down.svg';
+import { urlToHttpOptions } from 'url';
+
+const Select = styled.select`
+  font-family: 'Regular';
+  font-size: 1.3rem;
+  border: none;
+  width: 100%;
+  height: 100%;
+  background-color: ${palette.inversed1};
+  -webkit-appearance: none; /* for chrome */
+  -moz-appearance: none; /*for firefox*/
+  appearance: none;
+`;
+
+const SelectWrap = styled.div`
+  width: 12rem;
+  padding: 0.8rem 1.6rem;
+  background-color: ${palette.inversed1};
+  border-radius: 1.2rem;
+  position: relative;
+  & > svg {
+    position: absolute;
+    right: 1.6rem;
+  }
+`;
+
+interface SelectboxProps {
+  children: ReactNode;
+  options?: string[];
+}
+
+const Selectbox = ({ children, options }: SelectboxProps) => {
+  return (
+    <SelectWrap>
+      <Down width="1.6rem" height="1.6rem" />
+      <Select>
+        <option>{children}</option>
+        {options && options.map((option) => <option>{option}</option>)}
+      </Select>
+    </SelectWrap>
+  );
+};
+
+export default Selectbox;
