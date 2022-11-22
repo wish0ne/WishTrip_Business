cf746624c9eff282dd07617f1a1a72e012bf06ac
wish0ne <wish@khu.ac.kr>
2022-11-22 23:44:54 +0900

    feat: api 함수 작성

--- Frontend/src/lib/api/client.js
+++ Frontend/src/lib/api/client.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
 
 const client = axios.create();
+client.defaults.baseURL = '/api/store/';
+
+//대분류 목록 불러오기
+export const mainCategory = () => {
+  client.get('category/main');
+};
+
+//중분류 목록 불러오기
+export const middleCategory = (code) => {
+  client.get(`category/middle/${code}`);
+};
+
+//소분류 목록 불러오기
+export const subCategory = (code) => {
+  client.get(`category/sub/${code}`);
+};
 
 export default client;
